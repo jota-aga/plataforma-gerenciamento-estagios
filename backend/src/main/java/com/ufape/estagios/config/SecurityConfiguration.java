97ca91c8427941145bd5ccabacc413bcab7316a5
Pedro Henrique <pedrohenriquemo2020@gmail.com>
2026-02-17 10:54:35 -0300

    change import for Customizer

--- backend/src/main/java/com/ufape/estagios/config/SecurityConfiguration.java
+++ backend/src/main/java/com/ufape/estagios/config/SecurityConfiguration.java
@@ -1,11 +1,10 @@
 package com.ufape.estagios.config;
 
-import java.beans.Customizer;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
